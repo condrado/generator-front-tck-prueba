656797b6344a56d5927dd5c2b2d15a5c43ccbe7c
conrado <conrado.ramos@the-cocktail.com>
2022-11-20 13:20:51 +0100

    sass

--- app/index.js
+++ app/index.js
@@ -10,19 +10,29 @@ module.exports = class extends Generator {
 
   // Async Await
   async prompting() {
-    this.answers = await this.prompt([{
-      type: 'input',
-      name: 'name',
-      message: 'Your project name::',
-      default: this.appname, // appname return the default folder name to project
-      store: true,
-    },
-    {
-      type: 'list',
-      name: 'templateType',
-      message: 'Select the template wanted::',
-      choices: ['Front-End React', 'Node API builder', 'FullStack Application']
-    }]);
+    this.answers = await this.prompt(
+      [
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Nombre del proyecto:',
+          default: this.appname, // appname return the default folder name to project
+          store: true,
+        },
+        {
+          type: 'list',
+          name: 'templateType',
+          message: 'Selleciona la semilla que deseas utilizar:',
+          choices: ['HTML5, CSS3 y Javascript V01', 'Node API builder', 'FullStack Application']
+        },
+        {
+          type: 'list',
+          name: 'sassType',
+          message: 'Selleciona la arquitectura de SASS que deseas utilizar:',
+          choices: ['ITCSS', 'SMACSS']
+        }
+      ]
+    );
   }
 
   // install() {
@@ -30,8 +40,8 @@ module.exports = class extends Generator {
   // }
 
   writing() {
-    if (this.answers.templateType === 'Front-End React') {
-      this._writingReactTemplate();
+    if (this.answers.templateType === 'HTML5, CSS3 y Javascript V01') {
+      this._writingHtmlCssJsV01Template();
     } else if (this.answers.templateType === 'Node API builder') {
       this._writingApiTemplate()
     }
@@ -41,6 +51,7 @@ module.exports = class extends Generator {
     }
   }
 
+  // Template Type
   _writingReactTemplate() {
     this.fs.copy(
       this.templatePath('frontend'),
@@ -50,7 +61,6 @@ module.exports = class extends Generator {
       this.templatePath('frontend/public/index.html'),
       this.destinationPath('frontend/public/index.html'),
       { title: this.answers.name } // Embedded JavaScript templating.
-
     )
   }
 
@@ -59,6 +69,38 @@ module.exports = class extends Generator {
       this.templatePath('api'),
       this.destinationPath(this.answers.name +'-api')
     )
+  }
+
+  _writingHtmlCssJsV01Template() {
+    this.fs.copy(
+      this.templatePath('html-css-js-v01'),
+      this.destinationPath(this.answers.name)
+    )
+
+    // if (this.answers.sassType === 'ITCSS') {
+    //   this._writingArqITCSSTemplate() 
+    // } else {
+    //   this._writingArqSMACSSTemplate() 
+    // }
+  }
+
+  // Arquitecture Type
+  _writingArqITCSSTemplate() {
+    this.fs.copyTpl(
+      this.templatePath('html-css-js-v01/scss/scss-itcss'),
+      this.destinationPath(this.answers.name + '/src')
+    )
+
+    this.fs.delete(this.answers.name + '/scss');
+  }
+
+  _writingArqSMACSSTemplate() {
+    this.fs.copyTpl(
+      this.templatePath('html-css-js-v01/scss/scss-smacss/**/*'),
+      this.destinationPath(this.answers.name + '/src')
+    )
+    
+    this.fs.delete(this.answers.name + '/scss');
   }
 
   end() {
